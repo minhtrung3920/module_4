a843839bb57bfadbc591acf64191f71143893d35
187PM14029 - Trinh Minh Trung - K24T-PM1 <trung.187pm14029@vanlanguni.vn>
2021-10-05 17:14:05 +0700

    init commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+(function () {
+
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    
+    console.log("Hello " + names[0]);
+    console.log("Bye " + names[1]);
+    console.log("Bye " + names[2]);
+    console.log("Bye " + names[3]);
+    console.log("Bye " + names[4]);
+    console.log("Hello " + names[5]);
+    console.log("Hello " + names[6]);
+    console.log("Hello " + names[7]);
+    console.log("Hello " + names[8]);
+    console.log("Bye " + names[9]);
+
+    })();
